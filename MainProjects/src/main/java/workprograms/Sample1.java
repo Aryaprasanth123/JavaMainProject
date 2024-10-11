ed264f362cfbefa8a17315abeec22b9b994f0068
user <user@DESKTOP-UP2Q5NF>
2024-10-11 10:50:44 +0530

    completed projects

---
+++ MainProjects/src/main/java/workprograms/Sample1.java
@@ -0,0 +1,14 @@
+package workprograms;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Sample1.display();
+	}
+	public static void display()
+	{
+		System.out.println("Hello world");
+	}
+
+}
