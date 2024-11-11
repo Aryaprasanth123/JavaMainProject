83529af874e532af6adaaf5d419d27adcb975ee8
user <user@DESKTOP-UP2Q5NF>
2024-11-11 21:04:37 +0530

    xpath completed

--- Automation/src/main/java/testscript/InputMethodfield.java
+++ Automation/src/main/java/testscript/InputMethodfield.java
@@ -6,16 +6,16 @@ import org.openqa.selenium.WebElement;
 public class InputMethodfield extends Base {
 	public void getMessage()
 	{
-		WebElement input=driver.findElement(By.xpath("//a[@href='single-form-demo.php']"));
+		WebElement input=driver.findElement(By.xpath("//a[@href='simple-form-demo.php']"));
 		input.click();
 		WebElement msg=driver.findElement(By.xpath("//input[@id='single-input-field']"));
 		msg.sendKeys("Welcome");
 		WebElement showmessage=driver.findElement(By.xpath("//button[@id='button-one']"));
 		showmessage.click();
 		WebElement value1=driver.findElement(By.xpath("//input[@id='value-a']"));
-		value1.click();
+		value1.sendKeys("20");
 		WebElement value2=driver.findElement(By.xpath("//input[@id='value-b']"));
-		value2.click();
+		value2.sendKeys("30");
 		WebElement total=driver.findElement(By.xpath("//button[@id='button-two']"));
 		total.click();
 	}
